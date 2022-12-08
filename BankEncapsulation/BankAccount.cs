f89c798d18976b7ad1b26ec434a503d982926327
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-08 16:59:14 -0500

    completed exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccountClass
+    {
+        //Create:
+        //A private field of type double named balance with a value of 0
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        //Define a method named GetBalance that will return the amount stored in the balance field
+
+        private double _balance = 0;
+        public void DepositMethod (double amount)
+        {
+            _balance += amount;
+        }
+        public double GetBalance ()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,19 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            //create a new instance of the BankAccount class. 
+            //Allow the user of the application to Deposit money and retrieve their balance through the console.
+
+            BankAccountClass bankAccountVar = new BankAccountClass();
+            Console.WriteLine($"Your current balance is: ${bankAccountVar.GetBalance():N2}.");
+            Console.WriteLine("Please enter the amount you would like to deposit into your account: ");
+            Console.Write("$");
+            string depositVar = Console.ReadLine();
+            bankAccountVar.DepositMethod(double.Parse(depositVar));
+            Console.WriteLine($"You deposited ${depositVar}.");
+            Console.WriteLine($"Your new balance is: ${bankAccountVar.GetBalance():N2}");
         }
     }
 }
